08708ae2135cb6e21f35b88f291f57687b6189a4
Daywalkercs <152685474+Daywalkercs@users.noreply.github.com>
2025-08-25 13:39:11 +0300

    automatic working

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -4,53 +4,33 @@ namespace Warehouse.Data
 {
     public class DbInitializer
     {
-        public static void Seed(ApplicationDbContext context)
+        public static void Seed(ApplicationDbContext db)
         {
-            var units = new List<UnitOfMeasurement>()
+            // Если таблица единиц пустая
+            if (!db.UnitsOfMeasurement.Any())
             {
-                new UnitOfMeasurement { Id = 1, Name = "Килограмм", Abbreviation = "кг"},
-                new UnitOfMeasurement { Id = 2, Name = "Литр", Abbreviation = "л"},
-                new UnitOfMeasurement { Id = 3, Name = "Штука", Abbreviation = "шт"}
-            };
-
-            foreach (var unit in units)
-            {
-                var exist = context.UnitsOfMeasurement.Find(unit.Id);
-                if (exist == null)
-                {
-                    context.UnitsOfMeasurement.Add(unit);
-                }
-                else
-                {
-                    exist.Name = unit.Name;
-                    exist.Abbreviation = unit.Abbreviation;
-                    context.UnitsOfMeasurement.Update(exist);
-                }
+                db.UnitsOfMeasurement.AddRange(
+                    new UnitOfMeasurement { Name = "Килограмм", Abbreviation = "кг" },
+                    new UnitOfMeasurement { Name = "Литр", Abbreviation = "л" },
+                    new UnitOfMeasurement { Name = "Штука", Abbreviation = "шт" }
+                );
+                db.SaveChanges(); // Сохраняем, чтобы Id сгенерировались
             }
 
-            var resources = new List<Resource>()
+            // Теперь можно добавлять ресурсы
+            if (!db.Resources.Any())
             {
-                new Resource { Id = 1, Name = "Кирпич", UnitOfMeasurementId = 1 },
-                new Resource { Id = 2, Name = "Кафель", UnitOfMeasurementId = 2 },
-                new Resource { Id = 3, Name = "Пластиковые панели", UnitOfMeasurementId = 3 },
-            };
+                var kg = db.UnitsOfMeasurement.First(u => u.Name == "Килограмм");
+                var l = db.UnitsOfMeasurement.First(u => u.Name == "Литр");
+                var sht = db.UnitsOfMeasurement.First(u => u.Name == "Штука");
 
-            foreach(var resource in resources)
-            {
-                var exist = context.Resources.Find(resource.Id);
-                if(exist == null)
-                {
-                    context.Resources.Add(resource);
-                }
-                else
-                {
-                    exist.Name = resource.Name;
-                    exist.UnitOfMeasurementId = resource.UnitOfMeasurementId;
-                    context.Resources.Update(exist);
-                }
+                db.Resources.AddRange(
+                    new Resource { Name = "Цемент", UnitOfMeasurementId = kg.Id },
+                    new Resource { Name = "Краска", UnitOfMeasurementId = l.Id },
+                    new Resource { Name = "Плитка", UnitOfMeasurementId = sht.Id }
+                );
+                db.SaveChanges();
             }
-            
-            context.SaveChanges();
         }
     }
 }
